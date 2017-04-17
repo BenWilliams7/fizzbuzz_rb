6b5cf245bc0a212c20776ae6f1b3fccec55a9abe
Ben Williams and Spencer Ruiz <spencer@>
2017-04-17 13:16:26 -0700

    Add refactor

--- pingpong.rb
+++ pingpong.rb
@@ -2,26 +2,6 @@ class Pingpong
   def inputValid?(number)
     !!number.to_s.match(/\A\d+(\0.0+)?\z/)
   end
-  # def countUpTo(number)
-  #   countArray = []
-  #   number.downto(1) {|n| countArray.unshift(n)}
-  #   countArray
-  # end
-  # def transformIntoPingPong(number)
-  #   countList = countUpTo(number)
-  #   countList.map do |n|
-  #     if n % 15 == 0
-  #       "pingpong"
-  #     elsif n % 3 == 0
-  #       "ping"
-  #     elsif n % 5 == 0
-  #       "pong"
-  #     else
-  #       n
-  #     end
-  #   end
-  # end
-
   def transformPingPong (number)
     (1..number).map do |num|
       if num % 15 == 0
@@ -35,7 +15,6 @@ class Pingpong
       end
     end
   end
-
   def doPingPongThing(number)
     if inputValid?(number)
       transformPingPong(number)
@@ -43,5 +22,24 @@ class Pingpong
       "It's not a valid number yo."
     end
   end
-
 end
+
+# def countUpTo(number)
+#   countArray = []
+#   number.downto(1) {|n| countArray.unshift(n)}
+#   countArray
+# end
+# def transformIntoPingPong(number)
+#   countList = countUpTo(number)
+#   countList.map do |n|
+#     if n % 15 == 0
+#       "pingpong"
+#     elsif n % 3 == 0
+#       "ping"
+#     elsif n % 5 == 0
+#       "pong"
+#     else
+#       n
+#     end
+#   end
+# end
--- pingpongspec.rb
+++ pingpongspec.rb
@@ -1,43 +1,30 @@
 require "rspec"
 require "./pingpong.rb"
 
+
 describe "Pingpong#inputValid?" do
-  it "returns false for negative numbers" do
+  begin
     pingpong = Pingpong.new
-    expect(pingpong.inputValid?(-3)).to(eq false )
-  end
-  it "returns false for non-integer numbers" do
-    pingpong = Pingpong.new
-    expect(pingpong.inputValid?(3.23)).to(eq false )
-  end
-  it "returns true for whole numbers" do
-    pingpong = Pingpong.new
-    expect(pingpong.inputValid?(11)).to(eq true )
-  end
-  it "returns false when passed nil" do
-    pingpong = Pingpong.new
-    expect(pingpong.inputValid?(nil)).to(eq false )
-  end
-  it "returns false when passed a non-integer string" do
-    pingpong = Pingpong.new
-    expect(pingpong.inputValid?("thebird")).to(eq false )
-  end
-  it "returns false when passed a string" do
-    pingpong = Pingpong.new
-    expect(pingpong.inputValid?("12")).to(eq true )
+    it "returns false for negative numbers" do
+      expect(pingpong.inputValid?(-3)).to(eq false )
+    end
+    it "returns false for non-integer numbers" do
+      expect(pingpong.inputValid?(3.23)).to(eq false )
+    end
+    it "returns true for whole numbers" do
+      expect(pingpong.inputValid?(11)).to(eq true )
+    end
+    it "returns false when passed nil" do
+      expect(pingpong.inputValid?(nil)).to(eq false )
+    end
+    it "returns false when passed a non-integer string" do
+      expect(pingpong.inputValid?("thebird")).to(eq false )
+    end
+    it "returns true when passed a string" do
+      expect(pingpong.inputValid?("12")).to(eq true )
+    end
   end
 end
-
-# describe "Pingpong#countUpTo" do
-#   xit "lists positive integers up to the input" do
-#     pingpong = Pingpong.new
-#     expect(pingpong.countUpTo(4)).to(eq([1,2,3,4]))
-#   end
-#   xit "returns empty array if given negative number" do
-#     pingpong = Pingpong.new
-#     expect(pingpong.countUpTo(-4)).to(eq([]))
-#   end
-# end
 
 describe "Pingpong#transformIntoPingPong" do
   it "replaces every 3rd number with ping" do
@@ -64,3 +51,15 @@ describe "Pingpong#doPingPongThing" do
     expect(pingpong.doPingPongThing(16)).to(eq([1,2,"ping",4,"pong","ping", 7, 8, "ping", "pong", 11, "ping", 13, 14, "pingpong",16]))
   end
 end
+
+
+# describe "Pingpong#countUpTo" do
+#   xit "lists positive integers up to the input" do
+#     pingpong = Pingpong.new
+#     expect(pingpong.countUpTo(4)).to(eq([1,2,3,4]))
+#   end
+#   xit "returns empty array if given negative number" do
+#     pingpong = Pingpong.new
+#     expect(pingpong.countUpTo(-4)).to(eq([]))
+#   end
+# end
